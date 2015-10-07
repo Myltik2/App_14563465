a63cd8c8643261cce522bb8f5973377040883811
–ѓ <chilimovpasha@gmail.com>
2015-10-07 14:39:31 +0300

    Initial Commit

---
+++ WeatherApp_1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WeatherApp_1
+//
+//  Created by Павел Чилимов on 07.10.15.
+//  Copyright © 2015 Павел Чилимов. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
